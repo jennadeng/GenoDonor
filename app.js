1aa58a95e1cc6c7940f34388f4059d7ac681215e
Arun Ranganathan <arun.ranga@hotmail.ca>
2018-03-10 18:41:47 -0800

    made /users/ endpoint to grab all the reports for user

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const session = require('express-session');
 const genomeLink = require('genomelink-node');
+var Promise = require("bluebird");
+var _ = require('lodash');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -20,7 +22,7 @@ app.get('/', async (req, res) => {
   const authorizeUrl = genomeLink.OAuth.authorizeUrl({ scope: scope });
 
   // Fetching a protected resource using an OAuth2 token if exists.
-  let reports = [];
+  var reports = [];
   if (req.session.oauthToken) {
     const scopes = scope.split(' ');
     reports = await Promise.all(scopes.map( async (name) => {
@@ -37,6 +39,49 @@ app.get('/', async (req, res) => {
     reports: reports,
   });
 });
+
+app.get('/users/:usertoken', async (req, res) => {
+  console.log(req.params)
+  var users = ['GENOMELINKTEST001','GENOMELINKTEST002','GENOMELINKTEST003','GENOMELINKTEST004','GENOMELINKTEST005','GENOMELINKTEST006','GENOMELINKTEST007','GENOMELINKTEST008','GENOMELINKTEST009','GENOMELINKTEST010'];
+
+  if (users.indexOf(req.params.usertoken)  == -1){
+    throw "User is not found!";
+    return;
+  }
+
+  var report_types = ['agreeableness','alcohol-drinking-behavior','alpha-linolenic-acid','anger','beard-thickness','beta-carotene','bitter-taste','black-hair','bmi','breast-cancer','caffeine-consumption','caffeine-metabolite-ratio','calcium','carbohydrate-intake','childhood-intelligence','colorectal-cancer','conscientiousness','depression','egg-allergy','endurance-performance','extraversion','eye-color','folate','freckles','gambling','gastric-cancer','handedness','harm-avoidance','hearing-function','iron','liver-cancer','lobe-size','longevity','lung-cancer','magnesium','male-pattern-baldness-aga','milk-allergy','morning-person','motion-sickness','myocardial-infarction','neuroticism','nicotine-dependence','novelty-seeking','openness','pancreatic-cancer','peanuts-allergy','phosphorus','prostate-cancer','protein-intake','reading-and-spelling-ability','red-hair','red-wine-liking','response-to-vitamin-e-supplementation','reward-dependence','skin-pigmentation','smell-sensitivity-for-malt','smoking-behavior','type-2-diabetes','vitamin-a','vitamin-b12','vitamin-d','vitamin-e','weight','white-wine-liking','word-reading-ability'];
+
+  var user_promise = _.map(report_types, makePromise);
+
+  function makePromise(n) {
+      return genomeLink.Report.fetch({
+          name: n,
+          population: 'european',
+          token: req.params.usertoken
+      })
+  }
+
+  function parseData (object1, data) {
+    var name = _.get(data,'_data.phenotype.url_name');
+    if (name != null) {
+        object1[name] = data._data.summary.score || 0;
+    }
+    return object1
+  }
+
+  result1 = {};
+
+  var user_data = Promise.all(user_promise)
+      .then((data) => {
+        _.reduce(data, parseData, result1);
+        res.json(result1);
+      })
+      .catch((err)=>{console.log(err)});
+
+});
+
+
+
 
 app.get('/callback', async (req, res) => {
   // The user has been redirected back from the provider to your registered
