a7bbf476bfe098e7294fae88554bc2bf8dda1656
Arun Ranganathan <arun.ranga@hotmail.ca>
2018-03-10 20:16:33 -0800

    works?

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 const genomeLink = require('genomelink-node');
 var Promise = require("bluebird");
 var _ = require('lodash');
+const users = ['GENOMELINKTEST001','GENOMELINKTEST002','GENOMELINKTEST003','GENOMELINKTEST004','GENOMELINKTEST005','GENOMELINKTEST006','GENOMELINKTEST007','GENOMELINKTEST008','GENOMELINKTEST009','GENOMELINKTEST010'];
+
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -39,44 +41,55 @@ app.get('/', async (req, res) => {
   });
 });
 
-app.get('/users/:usertoken', async (req, res) => {
-  console.log(req.params)
-  var users = ['GENOMELINKTEST001','GENOMELINKTEST002','GENOMELINKTEST003','GENOMELINKTEST004','GENOMELINKTEST005','GENOMELINKTEST006','GENOMELINKTEST007','GENOMELINKTEST008','GENOMELINKTEST009','GENOMELINKTEST010'];
+function getUserData(userName){
 
-  if (users.indexOf(req.params.usertoken)  == -1){
-    throw "User is not found!";
-    return;
-  }
+    var report_types = ['agreeableness','alcohol-drinking-behavior','alpha-linolenic-acid','anger','beard-thickness','beta-carotene','bitter-taste','black-hair','bmi','breast-cancer','caffeine-consumption','caffeine-metabolite-ratio','calcium','carbohydrate-intake','childhood-intelligence','colorectal-cancer','conscientiousness','depression','egg-allergy','endurance-performance','extraversion','eye-color','folate','freckles','gambling','gastric-cancer','handedness','harm-avoidance','hearing-function','iron','liver-cancer','lobe-size','longevity','lung-cancer','magnesium','male-pattern-baldness-aga','milk-allergy','morning-person','motion-sickness','myocardial-infarction','neuroticism','nicotine-dependence','novelty-seeking','openness','pancreatic-cancer','peanuts-allergy','phosphorus','prostate-cancer','protein-intake','reading-and-spelling-ability','red-hair','red-wine-liking','response-to-vitamin-e-supplementation','reward-dependence','skin-pigmentation','smell-sensitivity-for-malt','smoking-behavior','type-2-diabetes','vitamin-a','vitamin-b12','vitamin-d','vitamin-e','weight','white-wine-liking','word-reading-ability'];
 
-  var report_types = ['agreeableness','alcohol-drinking-behavior','alpha-linolenic-acid','anger','beard-thickness','beta-carotene','bitter-taste','black-hair','bmi','breast-cancer','caffeine-consumption','caffeine-metabolite-ratio','calcium','carbohydrate-intake','childhood-intelligence','colorectal-cancer','conscientiousness','depression','egg-allergy','endurance-performance','extraversion','eye-color','folate','freckles','gambling','gastric-cancer','handedness','harm-avoidance','hearing-function','iron','liver-cancer','lobe-size','longevity','lung-cancer','magnesium','male-pattern-baldness-aga','milk-allergy','morning-person','motion-sickness','myocardial-infarction','neuroticism','nicotine-dependence','novelty-seeking','openness','pancreatic-cancer','peanuts-allergy','phosphorus','prostate-cancer','protein-intake','reading-and-spelling-ability','red-hair','red-wine-liking','response-to-vitamin-e-supplementation','reward-dependence','skin-pigmentation','smell-sensitivity-for-malt','smoking-behavior','type-2-diabetes','vitamin-a','vitamin-b12','vitamin-d','vitamin-e','weight','white-wine-liking','word-reading-ability'];
+    var user_promise = _.map(report_types, makePromise);
 
-  var user_promise = _.map(report_types, makePromise);
-
-  function makePromise(n) {
-      return genomeLink.Report.fetch({
-          name: n,
-          population: 'european',
-          token: req.params.usertoken
-      })
-  }
-
-  function parseData (object1, data) {
-    var name = _.get(data,'_data.phenotype.url_name');
-    if (name != null) {
-        object1[name] = data._data.summary.score || 0;
+    function makePromise(n) {
+        return genomeLink.Report.fetch({
+            name: n,
+            population: 'european',
+            token: userName
+        })
     }
-    return object1
-  }
 
-  result1 = {};
+    function parseData (object1, data) {
+        var name = _.get(data,'_data.phenotype.url_name');
+        if (name != null) {
+            object1[name] = data._data.summary.score || 0;
+        }
+        return object1
+    }
 
-  var user_data = Promise.all(user_promise)
-      .then((data) => {
-        _.reduce(data, parseData, result1);
-        res.json(result1);
-      })
-      .catch((err)=>{console.log(err)});
+    result1 = {};
 
+    return Promise.all(user_promise)
+        .then((data) => {
+          _.reduce(data, parseData, result1);
+          obj ={};
+          obj[userName] = result1;
+          return obj
+        })
+        .catch((err)=>{console.log(err)});
+
+}
+
+results2 = {};
+
+for (var i =0; i< users.length; i++) {
+    console.log(users[i])
+    Promise.resolve(getUserData(users[i]))
+        .then((data)=>{
+          results2[users[i]] = data;
+    })
+    .catch((err)=>{console.log(err);});
+}
+
+app.get('/users', async (req, res) => {
+    console.log(results2)
+    res.json(results2);
 });
 
 
